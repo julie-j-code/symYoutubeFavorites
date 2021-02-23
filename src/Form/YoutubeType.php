a3e9de01295964f308f4ba7eb97e8220aa90856b
Julie J <jeannet.julie@gmail.com>
2021-02-23 23:14:36 +0100

    imports manquants dans YoutubeType

--- src/Form/YoutubeType.php
+++ src/Form/YoutubeType.php
@@ -6,6 +6,9 @@ use App\Entity\Youtube;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class YoutubeType extends AbstractType
 {
